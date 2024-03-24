b43a6e347fc079de04b9588886328c4e7f0edcae
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-03-24 19:49:03 +0100

    add tabs to main menu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {SchoolsTableComponent} from "../components/schools-table/schools-table.component";
+import TabsComponent from "../components/tabs/tabs.component";
+import TabComponent from "../components/tabs/tab.component";
 
 @Component({
   selector: 'mmz-main-menu',
   standalone: true,
   imports: [
-    SchoolsTableComponent
+    SchoolsTableComponent,
+    TabsComponent,
+    TabComponent
   ],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
