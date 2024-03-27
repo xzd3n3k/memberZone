020e7df2d19f58d1728d3484ba6d4eeb21177230
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-03-27 06:26:33 +0100

    implement ValueControlAccessor to text input

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -1,14 +1,62 @@
 import {Component, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'mmz-text-input',
   standalone: true,
   imports: [],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi:true,
+      useExisting: TextInputComponent
+    }
+  ],
   templateUrl: './text-input.component.html',
   styleUrl: './text-input.component.scss'
 })
-export class TextInputComponent {
+export class TextInputComponent implements ControlValueAccessor{
   @Input() size: 'small' | 'medium' | 'large' = 'small';
+  @Input() label?: string;
+  @Input() placeholder: string = '';
+
+  text: string = '';
+
+  touched = false;
+  disabled = false;
+
+  onInput(event: any) {
+    this.markAsTouched();
+    this.text = event.target.value;
+    this.onChange(this.text);
+  }
+
+  onChange = (text: string) => {};
+
+  onTouched = () => {};
+
+  writeValue(text: string) {
+    this.text = text;
+  }
+
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  setDisabledState(disabled: boolean) {
+    this.disabled = disabled;
+  }
 
   protected getSize(side: 'height' | 'width'): string {
     return `var(--text-input-${side}-${this.size})`;
