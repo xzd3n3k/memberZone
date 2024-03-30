6dc0ae1c1f3314d23cc0ae80b2a29789e33e3e49
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-03-30 11:59:59 +0100

    add more functions to api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,15 +7,28 @@ import {Observable} from "rxjs";
 })
 export class ApiService {
   constructor(private http: HttpClient) { }
-  getTest() {
-    return this.http.get<any[]>('http://127.0.0.1:8000/');
-  }
 
   getToken(): Observable<string> {
     return this.http.get<string>('http://127.0.0.1:8000/token');
   }
 
+  getSchools() {
+    return this.http.get<Array<School>>('http://127.0.0.1:8000/get-schools');
+  }
+
+  getSchool(id: string): Observable<School> {
+    return this.http.get<School>(`http://127.0.0.1:8000/get-school/${id}`);
+  }
+
+  updateSchool(id: string, school: School) {
+    return this.http.post<any>(`http://127.0.0.1:8000/update-school/${id}`, school)
+  }
+
   createSchool(school: School) {
     return this.http.post<any>('http://127.0.0.1:8000/create-school', school);
+  }
+
+  deleteSchool(registration_number: string) {
+    return this.http.delete(`http://127.0.0.1:8000/delete-school/${registration_number}`);
   }
 }
