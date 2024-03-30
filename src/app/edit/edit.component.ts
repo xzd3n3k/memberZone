60dce103dd10c835169bfd9c72fec69fb99c975d
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-03-30 11:59:41 +0100

    fix the way data has been loading to this component

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -27,6 +27,7 @@ import {take} from "rxjs";
   styleUrl: './edit.component.css'
 })
 export class EditComponent implements OnInit {
+
   private destroyRef = inject(DestroyRef);
   private apiService: ApiService = inject(ApiService);
 
@@ -41,27 +42,33 @@ export class EditComponent implements OnInit {
   });
 
   constructor(private route: ActivatedRoute, private router: Router) {
+
+    this.selected = {
+      registration_number: '',
+      name: '',
+      principal: '',
+      phone: '',
+      street: '',
+      zip_code: '',
+      post_code: '',
+      city: '',
+      country: '',
+      province: '',
+      email: '',
+      active: false,
+      payed: false
+    }
+
     this.id = this.route.snapshot.paramMap.get('id');
     if (!this.id) {
-      this.selected = {
-        registration_number: '',
-        name: '',
-        principal: '',
-        phone: '',
-        street: '',
-        zip_code: '',
-        post_code: '',
-        city: '',
-        country: '',
-        province: '',
-        email: '',
-        active: false,
-        payed: false
-      }
-
       this.buttonText = 'Vytvořit';
     } else {
-      // this.selected = this.data.find(school => school.registration_number === this.id)!;
+      this.apiService.getSchool(this.id)
+        .pipe(take(1))
+        .subscribe(school => {
+          this.selected = school;
+          console.log(school);
+        })
       this.buttonText = 'Uložit';
     }
   }
@@ -69,7 +76,6 @@ export class EditComponent implements OnInit {
   ngOnInit() {
     // this.id = this.route.snapshot.paramMap.get('id');
     // this.selected = this.data.find(school => school.registration_number === this.id)!;
-    this.apiService.getTest().pipe(takeUntilDestroyed(this.destroyRef)).subscribe(value => console.log(value));
   }
 
   protected onFormSubmit() {
@@ -78,14 +84,27 @@ export class EditComponent implements OnInit {
     //   this.apiService.createSchool(this.selected!).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(value => console.log(value));
     // });
 
-    this.apiService.createSchool(this.selected!)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(value => {
-        if (value.message !== 'School created successfully!') {
-          console.warn('Some fields are empty!'); // TODO xzd3n3k toasts
-        } else {
-          this.router.navigateByUrl('/');
-        }
-      });
+    if (!this.id) {
+      this.apiService.createSchool(this.selected!)
+        .pipe(take(1))
+        .subscribe(value => {
+          if (value.message !== 'School created successfully!') {
+            console.warn('Some fields are empty!'); // TODO xzd3n3k toasts
+          } else {
+            this.router.navigateByUrl('/');
+          }
+        });
+    } else {
+      this.apiService.updateSchool(this.id, this.selected!)
+        .pipe(take(1))
+        .subscribe((value: any) => {
+          if (value.message !== 'School update successfully!') {
+            console.warn('Some fields are empty!');
+            console.warn(value);
+          } else {
+            this.router.navigateByUrl('/');
+          }
+        })
+    }
   }
 }
