d0b8935511ab6da0332e8f7d8988a4d8cb62eef9
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-04-05 19:13:22 +0200

    remove console log and comment

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -24,15 +24,11 @@ export default class TabsComponent implements AfterContentInit {
   private activeTabService = inject(ActiveTabService);
 
   ngAfterContentInit() {
-    console.log('aftercontentinit');
     const activeTabs = this.tabs.filter(tab => tab.active);
     this.activeTabService.activeTab$.subscribe(activeTab => {
       (activeTab.length > 0) ? this.selectTab(this.tabs.find(tab => tab.tabTitle === activeTab)!) : this.selectTab(this.tabs.first);
     })
 
-    // if (activeTabs.length === 0) {
-    //   this.selectTab(this.tabs.first);
-    // }
   }
 
   selectTab(tab: TabComponent) {
