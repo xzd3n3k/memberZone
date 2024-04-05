190b265c0c668ee975271717ab07effa737d82a2
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-04-05 17:28:47 +0200

    add service into app config and use it

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {ApiService} from "./api.service";
 import {HttpClientModule} from "@angular/common/http";
+import {ActiveTabService} from "./active-tab.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     {provide: ApiService, useClass: ApiService},
+    {provide: ActiveTabService, useClass: ActiveTabService},
     importProvidersFrom(HttpClientModule),
   ],
 };
--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -2,11 +2,12 @@ import {
   Component,
   ContentChildren,
   QueryList,
-  AfterContentInit
+  AfterContentInit, inject
 } from "@angular/core";
 
 import TabComponent from "./tab.component";
 import {NgClass, NgForOf} from "@angular/common";
+import {ActiveTabService} from "../../active-tab.service";
 
 @Component({
   selector: "mmz-tabs",
@@ -20,16 +21,22 @@ import {NgClass, NgForOf} from "@angular/common";
 })
 export default class TabsComponent implements AfterContentInit {
   @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
+  private activeTabService = inject(ActiveTabService);
 
   ngAfterContentInit() {
+    console.log('aftercontentinit');
     const activeTabs = this.tabs.filter(tab => tab.active);
+    this.activeTabService.activeTab$.subscribe(activeTab => {
+      (activeTab.length > 0) ? this.selectTab(this.tabs.find(tab => tab.tabTitle === activeTab)!) : this.selectTab(this.tabs.first);
+    })
 
-    if (activeTabs.length === 0) {
-      this.selectTab(this.tabs.first);
-    }
+    // if (activeTabs.length === 0) {
+    //   this.selectTab(this.tabs.first);
+    // }
   }
 
   selectTab(tab: TabComponent) {
+    this.activeTabService.markTabAsActive(tab.tabTitle);
     this.tabs.toArray().forEach(tab => (tab.active = false));
     tab.active = true;
   }
