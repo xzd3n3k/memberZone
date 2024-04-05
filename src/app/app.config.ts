c9d10a664b2d3c58465654b91a8973c401870eaf
Zdeněk Němec <xzdeneknemec@outlook.cz>
2024-04-05 19:12:51 +0200

    add service into app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { routes } from './app.routes';
 import {ApiService} from "./api.service";
 import {HttpClientModule} from "@angular/common/http";
 import {ActiveTabService} from "./active-tab.service";
+import {LoadingService} from "./loading.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     {provide: ApiService, useClass: ApiService},
     {provide: ActiveTabService, useClass: ActiveTabService},
+    {provide: LoadingService, useClass: LoadingService},
     importProvidersFrom(HttpClientModule),
   ],
 };
